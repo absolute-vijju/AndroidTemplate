07bc701d361a7a281937d096c26d29880cfabd13
Vijay Koshti <vijay.koshti@ekaratech.com>
2021-09-17 15:43:34 +0530

    Custom shaped added(Quadrilateral,Star,etc.)

--- app/src/main/java/com/example/androidtemplate/my_views/MyCustomView.kt
+++ app/src/main/java/com/example/androidtemplate/my_views/MyCustomView.kt
@@ -1,42 +1,144 @@
 package com.example.androidtemplate.my_views
 
-import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
-import java.text.DateFormat
-import java.util.*
-
 
 class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val path = Path()
+    private val paint = Paint().apply {
         color = Color.RED
         style = Paint.Style.STROKE
-        strokeWidth = 6f
+        strokeWidth = 12f
+        isAntiAlias = true
+        strokeCap = Paint.Cap.SQUARE
     }
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.apply {
-//            drawLine(0f, 0f, 100f, 200f, paint)
-//            drawCircle(300f, 300f, 100f, paint)
-//            drawText("Vijay Koshti", 300f, 300f, paint)
-//            drawRect(100f, 100f, 300f, 200f, paint)
-//            drawRoundRect(100f, 100f, 300f, 200f, 10f, 10f, paint)
-
-            drawLine(200f, 0f, 400f, 200f, paint)    // \
-            drawLine(400f, 200f, 400f, 400f, paint)  // |
-            drawLine(400f, 400f, 200f, 600f, paint)  // /
-            drawLine(200f, 600f, 0f, 400f, paint)    // \
-            drawLine(0f, 400f, 0f, 200f, paint)      // |
-            drawLine(0f, 200f, 200f, 0f, paint)      // /
+            drawColor(Color.YELLOW)    // Background color of Canvas
+            simpleLine(this)
+            quadrilateral(this)
+            howPaddingWorks(this)
+            drawStar(this)
         }
-            DateFormat.getTimeInstance(DateFormat.SHORT).format(Date().time)
     }
 
+    private fun drawStar(canvas: Canvas?) {
+        canvas?.apply {
+            var x = width.toFloat()
+            var y = height.toFloat()
+
+            val paddingRight = 16f + paddingRight
+            val paddingLeft = 16f + paddingLeft
+            val paddingTop = 16f + paddingTop
+            val paddingBottom = 16f + paddingBottom
+
+            val centerX = x / 2
+            val centerY = y / 2
+
+            x -= paddingRight
+            y -= paddingBottom
+
+            path.apply {
+                moveTo(centerX, paddingTop)
+                lineTo(x, y)
+                lineTo(paddingLeft, centerY)
+                lineTo(x, centerY)
+                lineTo(paddingLeft,y)
+                lineTo(centerX, paddingTop)
+
+                drawPath(this,paint)
+            }
+
+        }
+    }
+
+    private fun simpleLine(canvas: Canvas?) {
+        canvas?.apply {
+            var x = width.toFloat()
+            var y = height.toFloat()
+
+            val paddingRight = 16f + paddingRight
+            val paddingLeft = 16f + paddingLeft
+            val paddingTop = 16f + paddingTop
+            val paddingBottom = 16f + paddingBottom
+
+            val centerX = x / 2
+            val centerY = y / 2
+
+            x -= paddingRight
+            y -= paddingBottom
+
+            path.moveTo(centerX, paddingTop)
+            path.lineTo(centerX, y)
+
+            drawPath(path, paint)
+
+        }
+    }
+
+    private fun createPath(sides: Int = 6, radius: Float = 100f): Path {
+        val cx = (width / 2).toFloat()
+        val cy = (height / 2).toFloat()
+
+        val path = Path()
+        val angle = 2.0 * Math.PI / sides
+        path.moveTo(
+            cx + (radius * Math.cos(0.0)).toFloat(),
+            cy + (radius * Math.sin(0.0)).toFloat()
+        )
+        for (i in 1 until sides) {
+            path.lineTo(
+                cx + (radius * Math.cos(angle * i)).toFloat(),
+                cy + (radius * Math.sin(angle * i)).toFloat()
+            )
+        }
+        path.close()
+        return path
+    }
+
+    private fun quadrilateral(canvas: Canvas?) {
+        var x = width.toFloat()
+        var y = height.toFloat()
+
+        val paddingRight = 16f + paddingRight
+        val paddingLeft = 16f + paddingLeft
+        val paddingTop = 16f + paddingTop
+        val paddingBottom = 16f + paddingBottom
+
+        val centerX = x / 2
+        val centerY = y / 2
+
+        x -= paddingRight
+        y -= paddingBottom
+
+        canvas?.apply {
+            path.apply {
+                moveTo((centerX), (paddingTop))
+                lineTo(x, centerY)
+                lineTo(centerX, y)
+                lineTo(paddingLeft, centerY)
+                lineTo((centerX), (paddingTop))
+            }
+            drawPath(path, paint)
+        }
+    }
+
+    private fun howPaddingWorks(canvas: Canvas?) {
+        canvas?.apply {
+            val left = paddingLeft
+            val top = paddingTop
+            val right = width - paddingRight
+            val bottom = height - paddingBottom
+            drawLine(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat(), paint)
+        }
+    }
 
     /**
      *For drawRect(10f, 10f, 100f, 200f, paint)
@@ -59,6 +161,4 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
      *                                              *
      *
      */
-
-
 }
